8385c0ebd7a7c34a2cf8b725e733496d4c2941cb
Enoch Flourish <flourzeez@gmail.com>
2023-10-23 02:16:33 +0300

    creayed the header and adding the link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className="bg-slate-200">
-        <div>
-            <h1>
-            MY AUTHENTICATION APP
-            </h1>
+        <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
 
-            <ul>
+                    <Link to="/">
+            <h1 className="pl-4 font-bold">
+                MY AUTHENTICATION APP
+            </h1>
+            </Link>
+
+            <ul className=" flex flex-row gap-8 pr-2">
                 <li>
                     Home
                 </li>
                 <li>
                     About
+                </li>
+                <li>
+                    Sign In
                 </li>
             </ul>
         
