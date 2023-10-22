6454fa1baf5260e1b069d9a17cf52cd9096a3fe1
Enoch Flourish <flourzeez@gmail.com>
2023-10-22 03:35:53 +0300

    created pages and installed routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Profile />} />
+        <Route path="/profile" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
