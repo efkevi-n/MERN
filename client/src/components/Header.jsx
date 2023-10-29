bcaaeadabff33117d90047e2770981c4bac15a27
Enoch Flourish <flourzeez@gmail.com>
2023-10-29 15:02:28 +0300

    created the header section and installed reacticon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,40 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import {FaSearch} from 'react-icons/fa';
 
 export default function Header() {
   return (
-    <div className="bg-slate-200">
-        <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
+    <header className="bg-slate-200 shadow">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-6xl p-3">
+        <Link to="/">
+          <h1 className="flex flex-wrap font-bold pr-5 items-center">MY AUTHENTICATION APP</h1>
+          
+        </Link>
+        <form className="flex flex-row gap-2 items-center">
+            <input
+                type="text"
+                placeholder="Search..."
+                className="p-2 rounded-md bg-transparent border border-slate-400 focus:outline-none w-24 sm:w-64"
+            />
+            <FaSearch className="text-slate-600" />
+      
+        </form>
+      
+        <ul className="flex flex-row gap-5">
+          <Link to="/">
+            <li className='hidden sm:inline hover:underline' >Home</li>
+          </Link>
 
-                    <Link to="/">
-            <h1 className="pl-4 font-bold">
-                MY AUTHENTICATION APP
-            </h1>
-            </Link>
+          <Link to="/about">
+            <li className='hidden sm:inline hover:underline'>About</li>
+          </Link>
 
-            <ul className=" flex flex-row gap-8 pr-2">
-                <Link to="/">
-                <li>
-                    Home
-                </li>
-                </Link>
-
-                <Link to="/about">
-                <li>
-                    About
-                </li>
-                </Link>
-
-                <Link to="/signin">
-                <li>
-                    Sign In
-                </li>
-
-                </Link>
-            </ul>
-        
-        </div>
-    </div>
-  )
+          <Link to="/signin">
+            <li className='  hover:underline'>Sign In</li>
+          </Link>
+        </ul>
+    
+      </div>
+    </header>
+  );
 }
+
