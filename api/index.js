6d3986276c8c33c0933d0454b89f8fcba5f8a488
Enoch Flourish <flourzeez@gmail.com>
2023-10-29 16:07:29 +0300

    connected to the databse

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config(); // Load env variables
+
+
+mongoose.connect(process.env.MONGO).then(() =>  {
+    console.log('Connected to MongoDB');
+ }).catch((err) => { console.log(err) });   // Connect to MongoDB
+  
 const app = express();
 
 app.listen(3000, () => {
